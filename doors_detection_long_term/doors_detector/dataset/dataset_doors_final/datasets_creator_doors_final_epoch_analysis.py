ff6ea79551afd8de94aafc253e0beb23c767a525
micheleantonazzi <micheleantonazzi@gmail.com>
2022-06-06 16:01:19 +0200

    added scripts to analyze the train during epochs

---
+++ doors_detection_long_term/doors_detector/dataset/dataset_doors_final/datasets_creator_doors_final_epoch_analysis.py
@@ -0,0 +1,120 @@
+from typing import Union, Tuple, List
+
+import pandas as pd
+from sklearn.utils import shuffle
+
+from doors_detection_long_term.doors_detector.dataset.torch_dataset import TRAIN_SET, TEST_SET, SET
+from generic_dataset.dataset_manager import DatasetManager
+from doors_detection_long_term.doors_detector.dataset.dataset_doors_final.door_sample import DoorSample, DOOR_LABELS
+from sklearn.model_selection import train_test_split
+
+from doors_detection_long_term.doors_detector.dataset.dataset_doors_final.final_doors_dataset import DatasetDoorsFinal
+
+
+class DatasetsCreatorDoorsFinalEpochAnalysis:
+    def __init__(self, dataset_path: str):
+        self._dataset_path = dataset_path
+        self._dataset_manager = DatasetManager(dataset_path=dataset_path, sample_class=DoorSample)
+        self._dataframe = self._dataset_manager.get_dataframe()
+
+        self._experiment = 1
+        self._folder_name = None
+        self._use_negatives = False
+
+    def get_labels(self):
+        return DOOR_LABELS
+
+    def set_experiment_number(self, experiment: int, folder_name: str) -> 'DatasetsCreatorDoorsFinal':
+        """
+        This method is used to set up the experiment to run.
+        1) This first experiment consists on training a general model on a set of environment E \ {e}
+            the model is validated using a subset of the examples collected in E \ {e}
+            and it is tested using the 25% of the examples collected in e
+        2) The second experiment involves fine-tuning the previously trained model using some examples of the test data used in experiment 1.
+        This new training data belongs to a new environment, never seen in the first training phase. The remaining sample of the k-th folder are used as a test set.
+        :param experiment: the number of the experiment to perform. It's value must be 0 or 1
+        :param folder_name: the name of the folder to use as a test set in experiment 1 or to split into training a test sets in experiment 2.
+        :return: the instance of DatasetsCreatorDoorsFinal
+        """
+        assert experiment == 1 or experiment == 2
+
+        self._experiment = experiment
+        self._folder_name = folder_name
+        return self
+
+    def use_negatives(self, use_negatives: bool) -> 'DatasetsCreatorDoorsFinal':
+        """
+        Sets the presence of the negative samples in the test set.
+        :param use_negatives: True for including the negatives samples (samples with no doors) in the test set, False to use only positives ones
+        :return: the instance of DatasetsDoorsF
+        """
+        self._use_negatives = use_negatives
+        return self
+
+    def create_datasets(self, train_size: float = 0.1, random_state: int = 42) -> List[DatasetDoorsFinal]:
+        """
+        This method returns the training and test sets.
+        :param train_size: the size of the training set in experiment 2. For the first experiment this parameter is not considered, all samples of folders k-1 are considered.
+        """
+        if isinstance(train_size, float):
+            assert 0.0 < train_size < 1.0
+
+        shuffled_dataframe = shuffle(self._dataframe, random_state=random_state)
+
+        if self._experiment == 1:
+            # Extract the dataframe for training the generic model (with the examples acquired in the set of environment E \ {e}
+            generic_dataframe = shuffled_dataframe[(shuffled_dataframe.folder_name != self._folder_name)]
+            train_index, validation_index = train_test_split(generic_dataframe.index.tolist(), train_size=0.01, test_size=0.01, random_state=random_state)
+
+            train_dataframe_generic = generic_dataframe.loc[train_index]
+            # Remove negative images for training
+            train_dataframe = train_dataframe_generic[shuffled_dataframe.label == 1]
+
+            validation_dataframe = generic_dataframe.loc[validation_index]
+
+            # Extract the dataframe for testing in the environment e (not used during the training)
+            qualified_dataframe = shuffled_dataframe[shuffled_dataframe.folder_name == self._folder_name]
+            _, test_index= train_test_split(qualified_dataframe.index.tolist(), test_size=0.25, random_state=random_state)
+
+            test_dataframe = qualified_dataframe.loc[test_index]
+
+            if not self._use_negatives:
+                validation_dataframe = validation_dataframe[validation_dataframe.label == 1]
+                test_dataframe = test_dataframe[test_dataframe.label == 1]
+
+        elif self._experiment == 2:
+            shuffled_dataframe = shuffled_dataframe[shuffled_dataframe.folder_name == self._folder_name]
+            positive_dataframe = shuffled_dataframe[shuffled_dataframe.label == 1]
+            negative_dataframe = shuffled_dataframe[shuffled_dataframe.label == 0]
+            train, test = train_test_split(positive_dataframe.index.tolist(), test_size=0.25, random_state=random_state)
+
+            if train_size < 0.75:
+                train, _ = train_test_split(train, train_size=train_size * (4 / 3), random_state=random_state)
+
+            train_dataframe = shuffled_dataframe.loc[train]
+            test_dataframe = shuffle(pd.concat([shuffled_dataframe.loc[test], negative_dataframe]), random_state=random_state)
+
+            if not self._use_negatives:
+                test_dataframe = test_dataframe[test_dataframe.label == 1]
+
+        def print_information(dataframe):
+            print(f'    - total samples = {len(dataframe.index)}\n'
+                  f'    - Folders considered: {sorted(dataframe.folder_name.unique())}\n'
+                  f'    - Labels considered: {sorted(dataframe.label.unique())}\n'
+                  f'    - Total samples in folder: ')
+            for folder in sorted(dataframe.folder_name.unique()):
+                print(f'        - {folder}: {len(dataframe[dataframe.folder_name == folder])} samples')
+                if DoorSample.GET_LABEL_SET():
+                    print(f'        Samples per label:')
+                    for label in sorted(list(DoorSample.GET_LABEL_SET())):
+                        print(f'            - {label}: {len(dataframe[(dataframe.folder_name == folder) & (dataframe.label == label)])}')
+            print()
+
+        for m, d in zip(['Datasets summary:', 'Train set summary:', 'Validation set summary', 'Test set summary:'], [self._dataframe, train_dataframe, validation_dataframe, test_dataframe]):
+            print(m)
+            print_information(d)
+
+
+        return (DatasetDoorsFinal(self._dataset_path, train_dataframe, TRAIN_SET, std_size=256, max_size=800, scales=[256 + i * 32 for i in range(11)]),
+                DatasetDoorsFinal(self._dataset_path, validation_dataframe, TEST_SET, std_size=256, max_size=800, scales=[256 + i * 32 for i in range(11)]),
+                DatasetDoorsFinal(self._dataset_path, test_dataframe, TEST_SET, std_size=256, max_size=800, scales=[256 + i * 32 for i in range(11)]))
--- doors_detection_long_term/doors_detector/utilities/plot.py
+++ doors_detection_long_term/doors_detector/utilities/plot.py
@@ -5,18 +5,28 @@ import matplotlib.pyplot as plt
 
 def plot_losses(logs: Dict[str, List[Dict[str, float]]], save_to_file: bool = False):
 
-    train_logs = logs['train']
-    test_logs = logs['test']
+    train_logs = logs.get('train', None)
+    train_after_backpropagation = logs.get('train_after_backpropagation', None)
+    validation_logs = logs.get('validation', None)
+    test_logs = logs.get('test', None)
 
     losses = list(train_logs[0].keys())
 
     for loss in losses:
         plt.title(loss.capitalize())
 
-        # Print train logs
-        plt.plot([i for i in range(len(train_logs))], [log_epoch[loss] for log_epoch in train_logs], label='Train loss')
+        # Plot train loss after backprop if exist, otherwise print train loss during training
+        if train_after_backpropagation is not None:
+            plt.plot([i for i in range(len(train_after_backpropagation))], [log_epoch[loss] for log_epoch in train_after_backpropagation], label='Train loss after backprop')
+        else:
+            plt.plot([i for i in range(len(train_logs))], [log_epoch[loss] for log_epoch in train_logs], label='Train loss')
 
         # Print test logs
         plt.plot([i for i in range(len(test_logs))], [log_epoch[loss] for log_epoch in test_logs], label='Test loss')
+
+        # Plot validation loss if exists
+        if validation_logs is not None:
+            plt.plot([i for i in range(len(validation_logs))], [log_epoch[loss] for log_epoch in validation_logs], label='Validation loss')
+
         plt.legend()
         plt.show()
