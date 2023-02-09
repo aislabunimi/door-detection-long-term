e7e45cf5992c1c42affe4b17eefdc16696e40094
michele <micheleantonazzi@gmail.com>
2023-02-09 10:44:42 +0100

    yolov5: fixed training script

--- doors_detection_long_term/scripts/doors_detector/train_yolov5/train_custom_yolov5_doors_dataset.py
+++ doors_detection_long_term/scripts/doors_detector/train_yolov5/train_custom_yolov5_doors_dataset.py
@@ -221,8 +221,11 @@ if __name__ == '__main__':
         data_loader_validation = DataLoader(validation, batch_size=params['batch_size'], collate_fn=collate_fn_yolov5, drop_last=False, num_workers=4)
         data_loader_test = DataLoader(test, batch_size=params['batch_size'], collate_fn=collate_fn_yolov5, drop_last=False, num_workers=4)
 
-        model, criterion, lr_scheduler, optimizer, logs = prepare_model(globals()[f'EXP_1_{house}_2_LAYERS_BACKBONE_{epochs_general}_EPOCHS'.upper()], reload_model=True, restart_checkpoint=False)
-        model.set_description(globals()[f'EXP_2_{house}_{quantity}_{epochs_general}_GENERAL_2_LAYERS_BACKBONE_{epochs_qualified_detectors[0]}_EPOCHS'.upper()])
+        model, compute_loss, optimizer, scheduler, scaler, start_epoch, nl, nw, nb, amp, nbs, accumulate, lf, logs = prepare_model(globals()[f'EXP_1_{house}_{epochs_general_detector[0]}_EPOCHS'.upper()], reload_model=True, restart_checkpoint=False, epochs=epochs_general_detector[-1])
+        print_logs_every = 10
+        last_opt_step = -1
+        model.to('cuda')
+        model.set_description(globals()[f'EXP_2_{house}_EPOCHS_GD_{epochs_general}_QD_{epochs_general_detector[0]}_fine_tune_{fine_tune_quantity}'.upper()])
         print_logs_every = 10
 
         start_time = time.time()
@@ -230,192 +233,102 @@ if __name__ == '__main__':
         for epoch in range(epochs_qualified_detectors[-1]):
 
             temp_logs = {'train': [], 'train_after_backpropagation': [], 'validation': [], 'test': []}
-            accumulate_losses = {}
+            accumulate_loss = []
 
             model.train()
-            criterion.train()
+            optimizer.zero_grad()
+            for d, data in tqdm(enumerate(data_loader_train), total=len(data_loader_train), desc=f'{house} - Epoch {epoch} - Train model'):
+                ni = d + nb * epoch
 
-            for i, training_data in enumerate(data_loader_train):
-                # Data is a tuple where
-                #   - data[0]: a tensor containing all images shape = [batch_size, channels, img_height, img_width]
-                #   - data[1]: a tuple of dictionaries containing the images' targets
+                # warmup
+                if ni <= nw:
+                    xi = [0, nw]  # x interp
+                    accumulate = max(1, np.interp(ni, xi, [1, nbs / params['batch_size']]).round())
+                    for j, x in enumerate(optimizer.param_groups):
+                        # bias lr falls from 0.1 to lr0, all other lrs rise from 0.0 to lr0
+                        x['lr'] = np.interp(ni, xi, [model.hyp['warmup_bias_lr'] if j == 0 else 0.0, x['initial_lr'] * lf(epoch)])
+                        if 'momentum' in x:
+                            x['momentum'] = np.interp(ni, xi, [model.hyp['warmup_momentum'], model.hyp['momentum']])
 
-                images, targets = training_data
-                images = images.to(device)
+                images, targets, converted_boxes = data
+                images = images.to('cuda')
 
-                # Move targets to device
-                targets = [{k: v.to(device) for k, v in target.items() if k != 'folder_name' and k != 'absolute_count'} for target in targets]
+                with torch.cuda.amp.autocast(amp):
+                    output = model(images)  # forward
+                    loss, loss_items = compute_loss(output, converted_boxes.to('cuda'))
 
-                outputs = model(images)
+                scaler.scale(loss).backward()
 
-                # Compute losses
-                losses_dict = criterion(outputs, targets)
-                weight_dict = criterion.weight_dict
+                if ni - last_opt_step >= accumulate:
+                    scaler.unscale_(optimizer)  # unscale gradients
+                    torch.nn.utils.clip_grad_norm_(model.model.parameters(), max_norm=10.0)  # clip gradients
+                    scaler.step(optimizer)  # optimizer.step
+                    scaler.update()
+                    optimizer.zero_grad()
+                    last_opt_step = ni
 
-                # Losses are weighted using parameters contained in a dictionary
-                losses = sum(losses_dict[k] * weight_dict[k] for k in losses_dict.keys() if k in weight_dict)
+                accumulate_loss.append(loss.item())
 
-                scaled_losses_dict = {k: v * weight_dict[k] for k, v in losses_dict.items() if k in weight_dict}
-                scaled_losses_dict['loss'] = sum(scaled_losses_dict.values())
-
-                # Back propagate the losses
-                optimizer.zero_grad()
-                losses.backward()
-                optimizer.step()
-
-                accumulate_losses = {k: scaled_losses_dict[k] if k not in accumulate_losses else sum([accumulate_losses[k], scaled_losses_dict[k]]) for k, _ in scaled_losses_dict.items()}
-
-                if i % print_logs_every == print_logs_every - 1:
-                    accumulate_losses = {k: v.item() / print_logs_every for k, v in accumulate_losses.items()}
-                    print(f'Train epoch [{epoch}] -> [{i}/{len(data_loader_train)}]: ' + ', '.join([f'{k}: {v}' for k, v in accumulate_losses.items()]))
-                    temp_logs['train'].append(accumulate_losses)
-                    accumulate_losses = {}
-
+            scheduler.step()
             logs['time'].append(time.time() - start_time)
             epoch_total = {}
             for d in temp_logs['train']:
                 for k in d:
                     epoch_total[k] = epoch_total.get(k, 0) + d[k]
 
-            logs['train'].append({k: v / len(temp_logs['train']) for k, v in epoch_total.items()})
-
-            print(f'----> EPOCH SUMMARY TRAIN [{epoch}] -> [{i}/{len(data_loader_train)}]: ' + ', '.join([f'{k}: {v}' for k, v in logs['train'][epoch].items()]))
+            logs['train'].append({'loss': sum(accumulate_loss, 0) / len(accumulate_loss)})
 
             # Train loss after backpropagation
             with torch.no_grad():
-                model.eval()
-                criterion.eval()
+                accumulate_loss = []
+                for i, data in tqdm(enumerate(data_loader_train), total=len(data_loader_train), desc=f'{house} - Epoch {epoch} - Test model with training data'):
+                    images, targets, converted_boxes = data
 
-                accumulate_losses = {}
-                for i, test_data in enumerate(data_loader_train):
-                    images, targets = test_data
-                    images = images.to(device)
+                    images = images.to('cuda')
+                    output = model(images)
+                    loss, loss_items = compute_loss(output, converted_boxes.to('cuda'))
+                    accumulate_loss.append(loss.item())
 
-                    # Move targets to device
-                    targets = [{k: v.to(device) for k, v in target.items() if k != 'folder_name' and k != 'absolute_count'} for target in targets]
+            logs['train_after_backpropagation'].append({'loss': sum(accumulate_loss, 0) / len(accumulate_loss)})
 
-                    outputs = model(images)
-
-                    # Compute losses
-                    losses_dict = criterion(outputs, targets)
-                    weight_dict = criterion.weight_dict
-
-                    # Losses are weighted using parameters contained in a dictionary
-                    losses = sum(losses_dict[k] * weight_dict[k] for k in losses_dict.keys() if k in weight_dict)
-
-                    scaled_losses_dict = {k: v * weight_dict[k] for k, v in losses_dict.items() if k in weight_dict}
-                    scaled_losses_dict['loss'] = sum(scaled_losses_dict.values())
-                    accumulate_losses = {k: scaled_losses_dict[k] if k not in accumulate_losses else sum([accumulate_losses[k], scaled_losses_dict[k]]) for k, _ in scaled_losses_dict.items()}
-
-                    if i % print_logs_every == print_logs_every - 1:
-                        accumulate_losses = {k: v.item() / print_logs_every for k, v in accumulate_losses.items()}
-                        print(f'Test after backprop epoch [{epoch}] -> [{i}/{len(data_loader_train)}]: ' + ', '.join([f'{k}: {v}' for k, v in accumulate_losses.items()]))
-                        temp_logs['train_after_backpropagation'].append(accumulate_losses)
-                        accumulate_losses = {}
-
-            epoch_total = {}
-            for d in temp_logs['train_after_backpropagation']:
-                for k in d:
-                    epoch_total[k] = epoch_total.get(k, 0) + d[k]
-
-            logs['train_after_backpropagation'].append({k: v / len(temp_logs['train_after_backpropagation']) for k, v in epoch_total.items()})
-
-            print(f'----> EPOCH SUMMARY TRAIN AFTER BACKPROP [{epoch}] -> [{i}/{len(data_loader_train)}]: ' + ', '.join([f'{k}: {v}' for k, v in logs['train_after_backpropagation'][epoch].items()]))
-
+            #print(f'----> EPOCH SUMMARY TRAIN AFTER BACKPROP [{epoch}] -> [{i}/{len(data_loader_train)}]: ' + ', '.join([f'{k}: {v}' for k, v in logs['train_after_backpropagation'][epoch].items()]))
 
             # Validation
             with torch.no_grad():
-                model.eval()
-                criterion.eval()
+                accumulate_loss = []
+                for i, data in tqdm(enumerate(data_loader_validation), total=len(data_loader_validation), desc=f'{house} - Epoch {epoch} - Test model with validation data'):
+                    images, targets, converted_boxes = data
 
-                accumulate_losses = {}
-                for i, test_data in enumerate(data_loader_validation):
-                    images, targets = test_data
-                    images = images.to(device)
+                    images = images.to('cuda')
+                    output = model(images)
+                    loss, loss_items = compute_loss(output, converted_boxes.to('cuda'))
+                    accumulate_loss.append(loss.item())
 
-                    # Move targets to device
-                    targets = [{k: v.to(device) for k, v in target.items() if k != 'folder_name' and k != 'absolute_count'} for target in targets]
-
-                    outputs = model(images)
-
-                    # Compute losses
-                    losses_dict = criterion(outputs, targets)
-                    weight_dict = criterion.weight_dict
-
-                    # Losses are weighted using parameters contained in a dictionary
-                    losses = sum(losses_dict[k] * weight_dict[k] for k in losses_dict.keys() if k in weight_dict)
-
-                    scaled_losses_dict = {k: v * weight_dict[k] for k, v in losses_dict.items() if k in weight_dict}
-                    scaled_losses_dict['loss'] = sum(scaled_losses_dict.values())
-                    accumulate_losses = {k: scaled_losses_dict[k] if k not in accumulate_losses else sum([accumulate_losses[k], scaled_losses_dict[k]]) for k, _ in scaled_losses_dict.items()}
-
-                    if i % print_logs_every == print_logs_every - 1:
-                        accumulate_losses = {k: v.item() / print_logs_every for k, v in accumulate_losses.items()}
-                        print(f'Validation epoch [{epoch}] -> [{i}/{len(data_loader_validation)}]: ' + ', '.join([f'{k}: {v}' for k, v in accumulate_losses.items()]))
-                        temp_logs['validation'].append(accumulate_losses)
-                        accumulate_losses = {}
-
-            epoch_total = {}
-            for d in temp_logs['validation']:
-                for k in d:
-                    epoch_total[k] = epoch_total.get(k, 0) + d[k]
-
-            logs['validation'].append({k: v / len(temp_logs['validation']) for k, v in epoch_total.items()})
-
-            print(f'----> EPOCH SUMMARY VALIDATION [{epoch}] -> [{i}/{len(data_loader_validation)}]: ' + ', '.join([f'{k}: {v}' for k, v in logs['validation'][epoch].items()]))
-
+            logs['validation'].append({'loss': sum(accumulate_loss, 0) / len(accumulate_loss)})
 
             # Test
             with torch.no_grad():
-                model.eval()
-                criterion.eval()
+                accumulate_loss = []
+                for i, data in tqdm(enumerate(data_loader_test), total=len(data_loader_test), desc=f'{house} - Epoch {epoch} - Test model with test data'):
+                    images, targets, converted_boxes = data
 
-                accumulate_losses = {}
-                for i, test_data in enumerate(data_loader_test):
-                    images, targets = test_data
-                    images = images.to(device)
+                    images = images.to('cuda')
+                    output = model(images)
+                    loss, loss_items = compute_loss(output, converted_boxes.to('cuda'))
+                    accumulate_loss.append(loss.item())
 
-                    # Move targets to device
-                    targets = [{k: v.to(device) for k, v in target.items() if k != 'folder_name' and k != 'absolute_count'} for target in targets]
-
-                    outputs = model(images)
-
-                    # Compute losses
-                    losses_dict = criterion(outputs, targets)
-                    weight_dict = criterion.weight_dict
-
-                    # Losses are weighted using parameters contained in a dictionary
-                    losses = sum(losses_dict[k] * weight_dict[k] for k in losses_dict.keys() if k in weight_dict)
-
-                    scaled_losses_dict = {k: v * weight_dict[k] for k, v in losses_dict.items() if k in weight_dict}
-                    scaled_losses_dict['loss'] = sum(scaled_losses_dict.values())
-                    accumulate_losses = {k: scaled_losses_dict[k] if k not in accumulate_losses else sum([accumulate_losses[k], scaled_losses_dict[k]]) for k, _ in scaled_losses_dict.items()}
-
-                    if i % print_logs_every == print_logs_every - 1:
-                        accumulate_losses = {k: v.item() / print_logs_every for k, v in accumulate_losses.items()}
-                        print(f'Test epoch [{epoch}] -> [{i}/{len(data_loader_test)}]: ' + ', '.join([f'{k}: {v}' for k, v in accumulate_losses.items()]))
-                        temp_logs['test'].append(accumulate_losses)
-                        accumulate_losses = {}
-
-            epoch_total = {}
-            for d in temp_logs['test']:
-                for k in d:
-                    epoch_total[k] = epoch_total.get(k, 0) + d[k]
-
-            logs['test'].append({k: v / len(temp_logs['test']) for k, v in epoch_total.items()})
-
-            print(f'----> EPOCH SUMMARY TEST [{epoch}] -> [{i}/{len(data_loader_test)}]: ' + ', '.join([f'{k}: {v}' for k, v in logs['test'][epoch].items()]))
-            #lr_scheduler.step()
+            logs['test'].append({'loss': sum(accumulate_loss, 0) / len(accumulate_loss)})
+            print(f'----> EPOCH {epoch} SUMMARY: ' + ', '.join([f'{k}: {v[epoch]}' for k, v in logs.items()]))
 
             plot_losses(logs)
 
             model.save(epoch=epoch,
                        optimizer_state_dict=optimizer.state_dict(),
-                       lr_scheduler_state_dict=lr_scheduler.state_dict(),
+                       lr_scheduler_state_dict=scheduler.state_dict(),
                        params=params,
                        logs=logs,
                        )
             # Change the model description on each epoch step
             if epoch == epochs_qualified_detectors[epoch_count] - 1 and epoch_count < len(epochs_qualified_detectors) -1:
                 epoch_count += 1
-                model.set_description(globals()[f'EXP_2_{house}_{quantity}_{epochs_general}_GENERAL_2_LAYERS_BACKBONE_{epochs_qualified_detectors[epoch_count]}_EPOCHS'.upper()])
+                model.set_description(globals()[f'EXP_2_{house}_EPOCHS_GD_{epochs_general}_QD_{epochs_general_detector[epoch_count]}_fine_tune_{fine_tune_quantity}'.upper()])
