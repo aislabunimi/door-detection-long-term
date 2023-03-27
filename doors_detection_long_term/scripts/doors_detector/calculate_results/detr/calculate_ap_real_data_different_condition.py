ad8830ac1e17fe5378dee3f8fcd771e9f30b0e95
michele <micheleantonazzi@gmail.com>
2023-03-27 10:45:06 +0200

    added script

---
+++ doors_detection_long_term/scripts/doors_detector/calculate_results/detr/calculate_ap_real_data_different_condition.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from doors_detection_long_term.doors_detector.models.detr_door_detector import *
+from doors_detection_long_term.doors_detector.dataset.torch_dataset import FINAL_DOORS_DATASET
+from doors_detection_long_term.doors_detector.evaluators.my_evaluator import MyEvaluator
+from doors_detection_long_term.doors_detector.models.detr_door_detector import DetrDoorDetector
+from doors_detection_long_term.doors_detector.models.model_names import DETR_RESNET50
+from doors_detection_long_term.doors_detector.utilities.utils import collate_fn
+from doors_detection_long_term.scripts.doors_detector.dataset_configurator import \
+    get_final_doors_dataset_epoch_analysis, get_final_doors_dataset_real_data
+
+houses = ['floor1', 'floor4',]# 'chemistry_floor0']
+epochs_general_detector = [40, 60]
+epochs_qualified_detector = [20, 40]
+fine_tune_quantity = [15, 25, 50, 75]
+datasets = ['GIBSON', 'DEEP_DOORS_2', 'GIBSON_DEEP_DOORS_2']
+device = 'cuda'
+
+
+def compute_results(model_name, data_loader_test, COLORS):
+    model = DetrDoorDetector(model_name=DETR_RESNET50, n_labels=len(labels.keys()), pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=model_name)
+    model.eval()
+    model.to(device)
+
+    evaluator = MyEvaluator()
+
+    for images, targets in tqdm(data_loader_test, total=len(data_loader_test), desc='Evaluate model'):
+        images = images.to(device)
+        outputs = model(images)
+        evaluator.add_predictions(targets=targets, predictions=outputs)
+
+    metrics = evaluator.get_metrics(iou_threshold=0.75, confidence_threshold=0.75, door_no_door_task=False, plot_curves=True, colors=COLORS)
+    mAP = 0
+    print('Results per bounding box:')
+    for label, values in sorted(metrics['per_bbox'].items(), key=lambda v: v[0]):
+        mAP += values['AP']
+        print(f'\tLabel {label} -> AP = {values["AP"]}, Total positives = {values["total_positives"]}, TP = {values["TP"]}, FP = {values["FP"]}')
+        print(f'\t\tPositives = {values["TP"] / values["total_positives"] * 100:.2f}%, False positives = {values["FP"] / (values["TP"] + values["FP"]) * 100:.2f}%')
+    print(f'\tmAP = {mAP / len(metrics["per_bbox"].keys())}')
+
+    return metrics
+
+
+def save_file(results, file_name):
+
+    results = np.array(results).T
+    columns = ['house', 'detector', 'dataset', 'epochs_gd', 'epochs_qd', 'label',  'AP', 'total_positives', 'TP', 'FP']
+    d = {}
+    for i, column in enumerate(columns):
+        d[column] = results[i]
+
+    dataframe = pd.DataFrame(d)
+
+    with pd.ExcelWriter('./../results/' + file_name) as writer:
+        if not dataframe.index.name:
+            dataframe.index.name = 'Index'
+        dataframe.to_excel(writer, sheet_name='s')
+
+results = []
+
+# General detectors
+for house, dataset, epochs_gd in [(h, d, e) for h in houses for d in datasets for e in epochs_general_detector]:
+
+    _, test, labels, COLORS = get_final_doors_dataset_real_data(folder_name=house + '_evening', train_size=0.25)
+    data_loader_test = DataLoader(test, batch_size=1, collate_fn=collate_fn, drop_last=False, num_workers=4)
+
+    model_name = globals()[f'EXP_GENERAL_DETECTOR_2_layers_BACKBONE_{dataset}_{epochs_gd}_EPOCHS'.upper()]
+
+    metrics = compute_results(model_name, data_loader_test, COLORS)
+
+    for label, values in sorted(metrics['per_bbox'].items(), key=lambda v: v[0]):
+        results += [[house.replace('_', ''), 'GD', dataset, epochs_gd, epochs_gd, label, values['AP'], values['total_positives'], values['TP'], values['FP']]]
+
+for house, dataset, epochs_gd, epochs_qd, fine_tune in [(h, d, e, eq) for h in houses for d in datasets for e in epochs_general_detector for eq in epochs_qualified_detector for ft in  fine_tune_quantity]:
+    _, test, labels, COLORS = get_final_doors_dataset_real_data(folder_name=house + '_evening', train_size=0.25)
+    data_loader_test = DataLoader(test, batch_size=1, collate_fn=collate_fn, drop_last=False, num_workers=4)
+
+    model_name = globals()[f'EXP_2_{house}_{dataset}_{epochs_gd}_FINE_TUNE_{fine_tune}_EPOCHS_{epochs_qd}'.upper()]
+
+    metrics = compute_results(model_name, data_loader_test, COLORS)
+
+    for label, values in sorted(metrics['per_bbox'].items(), key=lambda v: v[0]):
+        results += [[house.replace('_', ''), 'QD_' + str(fine_tune_quantity), dataset, epochs_gd, epochs_qd, label, values['AP'], values['total_positives'], values['TP'], values['FP']]]
+
+save_file(results, 'detr_ap_real_data_different_condition.xlsx')
