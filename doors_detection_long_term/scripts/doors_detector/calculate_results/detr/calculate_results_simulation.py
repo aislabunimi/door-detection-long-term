be0ec08d6c58d6cf2675466829c8d7590f9634e9
michele <micheleantonazzi@gmail.com>
2023-03-28 12:17:52 +0200

    calculate_results_ap_simulation.py: fixed

--- doors_detection_long_term/scripts/doors_detector/calculate_results/detr/calculate_results_simulation.py
+++ doors_detection_long_term/scripts/doors_detector/calculate_results/detr/calculate_results_simulation.py
@@ -3,6 +3,7 @@ import pandas as pd
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
+from doors_detection_long_term.doors_detector.evaluators.my_evaluators_complete_metric import MyEvaluatorCompleteMetric
 from doors_detection_long_term.doors_detector.models.detr_door_detector import *
 from doors_detection_long_term.doors_detector.dataset.torch_dataset import FINAL_DOORS_DATASET
 from doors_detection_long_term.doors_detector.evaluators.my_evaluator import MyEvaluator
@@ -24,13 +25,16 @@ def compute_results(model_name, data_loader_test, COLORS):
     model.to(device)
 
     evaluator = MyEvaluator()
+    evaluator_complete_metric =  MyEvaluatorCompleteMetric()
 
     for images, targets in tqdm(data_loader_test, total=len(data_loader_test), desc='Evaluate model'):
         images = images.to(device)
         outputs = model(images)
         evaluator.add_predictions(targets=targets, predictions=outputs)
+        evaluator_complete_metric.add_predictions(targets=targets, predictions=outputs)
 
     metrics = evaluator.get_metrics(iou_threshold=0.75, confidence_threshold=0.75, door_no_door_task=False, plot_curves=True, colors=COLORS)
+    complete_metrics = evaluator_complete_metric.get_metrics(iou_threshold=0.75, confidence_threshold=0.75, door_no_door_task=False, plot_curves=False, colors=COLORS)
     mAP = 0
     print('Results per bounding box:')
     for label, values in sorted(metrics['per_bbox'].items(), key=lambda v: v[0]):
@@ -39,32 +43,47 @@ def compute_results(model_name, data_loader_test, COLORS):
         print(f'\t\tPositives = {values["TP"] / values["total_positives"] * 100:.2f}%, False positives = {values["FP"] / (values["TP"] + values["FP"]) * 100:.2f}%')
     print(f'\tmAP = {mAP / len(metrics["per_bbox"].keys())}')
 
-    return metrics
+    return metrics, complete_metrics
 
 
-def save_file(results, file_name):
+def save_file(results, complete_results, file_name_1, file_name_2):
 
     results = np.array(results).T
-    columns = ['house', 'detector', 'epochs_gd', 'epochs', 'label',  'AP', 'total_positives', 'TP', 'FP']
+    columns = ['house', 'detector', 'epochs_gd', 'epochs_qd', 'label',  'AP', 'total_positives', 'TP', 'FP']
     d = {}
     for i, column in enumerate(columns):
         d[column] = results[i]
 
     dataframe = pd.DataFrame(d)
 
-    with pd.ExcelWriter('./../../../results/' + file_name) as writer:
+    with pd.ExcelWriter('./../../../results/' + file_name_1) as writer:
+        if not dataframe.index.name:
+            dataframe.index.name = 'Index'
+        dataframe.to_excel(writer, sheet_name='s')
+
+    complete_results = np.array(complete_results).T
+    columns = ['house', 'detector', 'epochs_gd', 'epochs_qd', 'label',  'total_positives', 'TP', 'FP', 'TPm', 'FPm', 'FPiou']
+    d = {}
+    for i, column in enumerate(columns):
+        d[column] = complete_results[i]
+
+    dataframe = pd.DataFrame(d)
+
+    with pd.ExcelWriter('./../../../results/' + file_name_2) as writer:
         if not dataframe.index.name:
             dataframe.index.name = 'Index'
         dataframe.to_excel(writer, sheet_name='s')
 
 
+results = []
+results_complete = []
+
+
 model_names_general_detectors = [(globals()[f'EXP_1_{house}_2_layers_BACKBONE_{epochs}_EPOCHS'.upper()], house, epochs) for house in houses for epochs in epochs_general_detector]
 model_names_qualified_detectors = [(globals()[f'EXP_2_{house}_{quantity}_{epochs_general}_GENERAL_2_layers_BACKBONE_{epochs_qualified}_EPOCHS'.upper()], house, quantity, epochs_general, epochs_qualified) for house in houses for quantity in fine_tune_quantity for epochs_general in epochs_general_detector for epochs_qualified in epochs_qualified_detector if not (quantity == 15 and epochs_general == 40)]
 
-results = []
 # General detectors
 for model_name, house, epochs in model_names_general_detectors:
-    print(model_name)
     _, _, test, labels, COLORS = get_final_doors_dataset_epoch_analysis(experiment=1, folder_name=house.replace('_', ''), train_size=0.25, use_negatives=False)
     data_loader_test = DataLoader(test, batch_size=1, collate_fn=collate_fn, drop_last=False, num_workers=4)
 
@@ -82,4 +101,4 @@ for model_name, house, quantity, epochs_general, epochs_qualified in model_names
     for label, values in sorted(metrics['per_bbox'].items(), key=lambda v: v[0]):
         results += [[house.replace('_', ''), 'QD_' + str(quantity), epochs_general, epochs_qualified, label, values['AP'], values['total_positives'], values['TP'], values['FP']]]
 
-save_file(results, 'detr_ap_simulation.xlsx')
+save_file(results, results_complete, 'detr_ap_simulation.xlsx', 'detr_complete_metric_simulation.xlsx')
