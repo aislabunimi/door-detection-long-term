e6b54fe6d17b103c5a312d971a124fb1ef183128
michele <micheleantonazzi@gmail.com>
2023-03-31 14:57:02 +0200

    plot_all_results_simulation.py: fixed

--- doors_detection_long_term/scripts/doors_detector/plot_results/detr/plot_all_results_simulation.py
+++ doors_detection_long_term/scripts/doors_detector/plot_results/detr/plot_all_results_simulation.py
@@ -7,15 +7,14 @@ from pandas import CategoricalDtype
 houses = pd.read_excel('./../../../results/detr_ap_simulation.xlsx')
 houses['AP'] = houses['AP'].astype(np.float64)
 houses['AP'] = houses['AP'].apply(lambda x: x*100).round()
-houses = houses.loc[(houses['epochs_gd'] == 60) & ((houses['epochs_qd'] == 40) | (houses['epochs_gd'] == 60))]
+houses = houses.loc[(houses['epochs_gd'] == 60) & ((houses['epochs_qd'] == 40) | (houses['epochs_qd'] == 60))]
 
 labels = ['$GD_{-e}$', '$QD^{25}_e$', '$QD^{50}_e$', '$QD^{75}_e$']
 experiments = ['GD', 'QD_25', 'QD_50', 'QD_75']
 
 houses_list_dtype = CategoricalDtype(
-    ['house1', 'house2', 'house7', 'house9', 'house10', 'house13', 'house15', 'house20', 'house21', 'house22'],
-    ordered=True
-)
+    ['house10', 'house2', 'house7', 'house9', 'house1', 'house13', 'house15', 'house20', 'house21', 'house22'],
+    ordered=True)
 
 closed_doors = houses[houses.label == 0][['house', 'detector', 'AP']]
 closed_doors = closed_doors.pivot_table(values=['AP'], index=closed_doors['house'], columns='detector', aggfunc='first').reset_index()
@@ -35,8 +34,7 @@ ax.bar(X, closed_doors[('AP', experiments[0])].tolist() + [closed_doors[('AP', e
 ax.bar(X + 0.2, closed_doors[('AP', experiments[1])].tolist() + [closed_doors[('AP', experiments[1])].mean()], yerr=np.array([[0, 0] for _ in range(10)] + [[closed_doors[('AP', experiments[1])].std(), closed_doors[('AP', experiments[1])].std()]]).T,  width=0.2, label=labels[1])
 ax.bar(X + 0.4, closed_doors[('AP', experiments[2])].tolist() + [closed_doors[('AP', experiments[2])].mean()], yerr=np.array([[0, 0] for _ in range(10)] + [[closed_doors[('AP', experiments[2])].std(), closed_doors[('AP', experiments[2])].std()]]).T, width=0.2, label=labels[2])
 ax.bar(X + 0.6, closed_doors[('AP', experiments[3])].tolist() + [closed_doors[('AP', experiments[3])].mean()], yerr=np.array([[0, 0] for _ in range(10)] + [[closed_doors[('AP', experiments[3])].std(), closed_doors[('AP', experiments[3])].std()]]).T, width=0.2, label=labels[3])
-
-ax.set_title('AP results over all houses - closed doors', fontsize=18)
+ax.set_title('Closed doors', fontsize=18)
 ax.set_ylim([0, 110])
 
 ax.tick_params(axis='y', labelsize=16)
@@ -62,7 +60,7 @@ ax.bar(X + 0.2, open_doors[('AP', experiments[1])].tolist() + [open_doors[('AP',
 ax.bar(X + 0.4, open_doors[('AP', experiments[2])].tolist() + [open_doors[('AP', experiments[2])].mean()], yerr=np.array([[0, 0] for _ in range(10)] + [[open_doors[('AP', experiments[2])].std(), open_doors[('AP', experiments[2])].std()]]).T, width=0.2, label=labels[2])
 ax.bar(X + 0.6, open_doors[('AP', experiments[3])].tolist() + [open_doors[('AP', experiments[3])].mean()], yerr=np.array([[0, 0] for _ in range(10)] + [[open_doors[('AP', experiments[3])].std(), open_doors[('AP', experiments[3])].std()]]).T, width=0.2, label=labels[3])
 
-ax.set_title('AP results over all houses - open doors', fontsize=18)
+ax.set_title('Open doors', fontsize=18)
 ax.set_ylim([0, 110])
 ax.tick_params(axis='y', labelsize=16)
 ax.set_xticks([i+0.3 for i in range(11)])
