fixed detr results calculation real data

doors_detection_long_term/scripts/doors_detector/bboxes_filtering/test.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 
 from doors_detection_long_term.doors_detector.dataset.torch_dataset import FINAL_DOORS_DATASET
-from doors_detection_long_term.doors_detector.models.BboxFilterNetwork import BboxFilterNetwork, TEST
+from doors_detection_long_term.doors_detector.models.bbox_filter_network import BboxFilterNetwork, TEST
 from doors_detection_long_term.doors_detector.models.model_names import BBOX_FILTER_NETWORK
 from doors_detection_long_term.doors_detector.utilities.utils import collate_fn_yolov5
 from doors_detection_long_term.scripts.doors_detector.dataset_configurator import \

doors_detection_long_term/scripts/doors_detector/calculate_results/detr/detr_calculate_results_real_data.py
@@ -9,7 +9,7 @@ from doors_detection_long_term.doors_detector.dataset.torch_dataset import FINAL
 from doors_detection_long_term.doors_detector.evaluators.my_evaluator import MyEvaluator
 from doors_detection_long_term.doors_detector.models.detr_door_detector import DetrDoorDetector
 from doors_detection_long_term.doors_detector.models.model_names import DETR_RESNET50
-from doors_detection_long_term.doors_detector.utilities.utils import collate_fn
+from doors_detection_long_term.doors_detector.utilities.utils import collate_fn, seed_everything
 from doors_detection_long_term.scripts.doors_detector.dataset_configurator import \
     get_final_doors_dataset_epoch_analysis, get_final_doors_dataset_real_data
 
@@ -20,6 +20,7 @@ fine_tune_quantity = [15, 25, 50, 75]
 datasets = ['GIBSON', 'DEEP_DOORS_2', 'GIBSON_DEEP_DOORS_2']
 device = 'cuda'
 
+seed_everything(seed=0)
 
 def compute_results(model_name, data_loader_test, COLORS):
     model = DetrDoorDetector(model_name=DETR_RESNET50, n_labels=len(labels.keys()), pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=model_name)
@@ -35,15 +36,14 @@ def compute_results(model_name, data_loader_test, COLORS):
         evaluator.add_predictions(targets=targets, predictions=outputs)
         evaluator_complete_metric.add_predictions(targets=targets, predictions=outputs)
 
-    metrics = evaluator.get_metrics(iou_threshold=0.75, confidence_threshold=0.75, door_no_door_task=False, plot_curves=False, colors=COLORS)
-    complete_metrics = evaluator_complete_metric.get_metrics(iou_threshold=0.75, confidence_threshold=0.75, door_no_door_task=False, plot_curves=False, colors=COLORS)
-    mAP = 0
-    print('Results per bounding box:')
-    for label, values in sorted(metrics['per_bbox'].items(), key=lambda v: v[0]):
-        mAP += values['AP']
-        print(f'\tLabel {label} -> AP = {values["AP"]}, Total positives = {values["total_positives"]}, TP = {values["TP"]}, FP = {values["FP"]}')
-        print(f'\t\tPositives = {values["TP"] / values["total_positives"] * 100:.2f}%, False positives = {values["FP"] / (values["TP"] + values["FP"]) * 100:.2f}%')
-    print(f'\tmAP = {mAP / len(metrics["per_bbox"].keys())}')
+    complete_metrics = {}
+    metrics = {}
+    for iou_threshold in np.arange(0.5, 0.96, 0.05):
+        for confidence_threshold in np.arange(0.5, 0.96, 0.05):
+            iou_threshold = round(iou_threshold, 2)
+            confidence_threshold = round(confidence_threshold, 2)
+            metrics[(iou_threshold, confidence_threshold)] = evaluator.get_metrics(iou_threshold=iou_threshold, confidence_threshold=confidence_threshold, door_no_door_task=False, plot_curves=False, colors=COLORS)
+            complete_metrics[(iou_threshold, confidence_threshold)] = evaluator_complete_metric.get_metrics(iou_threshold=iou_threshold, confidence_threshold=confidence_threshold, door_no_door_task=False, plot_curves=False, colors=COLORS)
 
     return metrics, complete_metrics
 
@@ -51,7 +51,7 @@ def compute_results(model_name, data_loader_test, COLORS):
 def save_file(results, complete_results, file_name_1, file_name_2):
 
     results = np.array(results).T
-    columns = ['house', 'detector', 'dataset', 'epochs_gd', 'epochs_qd', 'label',  'AP', 'total_positives', 'TP', 'FP']
+    columns = ['iou_threshold', 'confidence_threshold', 'house', 'detector', 'dataset', 'epochs_gd', 'epochs_qd', 'label',  'AP', 'total_positives', 'TP', 'FP']
     d = {}
     for i, column in enumerate(columns):
         d[column] = results[i]
@@ -64,7 +64,7 @@ def save_file(results, complete_results, file_name_1, file_name_2):
         dataframe.to_excel(writer, sheet_name='s')
 
     complete_results = np.array(complete_results).T
-    columns = ['house', 'detector', 'dataset', 'epochs_gd', 'epochs_qd', 'label',  'total_positives', 'TP', 'FP', 'TPm', 'FPm', 'FPiou']
+    columns = ['iou_threshold', 'confidence_threshold', 'house', 'detector', 'dataset', 'epochs_gd', 'epochs_qd', 'label',  'total_positives', 'TP', 'FP', 'TPm', 'FPm', 'FPiou']
     d = {}
     for i, column in enumerate(columns):
         d[column] = complete_results[i]
@@ -91,11 +91,13 @@ for house, dataset, epochs_gd in [(h, d, e) for h in houses for d in datasets fo
 
     metrics, complete_metrics = compute_results(model_name, data_loader_test, COLORS)
 
-    for label, values in sorted(metrics['per_bbox'].items(), key=lambda v: v[0]):
-        results += [[house.replace('_', ''), 'GD', dataset, epochs_gd, epochs_gd, label, values['AP'], values['total_positives'], values['TP'], values['FP']]]
+    for (iou_threshold, confidence_threshold), metric in metrics.items():
+        for label, values in sorted(metric['per_bbox'].items(), key=lambda v: v[0]):
+            results += [[iou_threshold, confidence_threshold, house.replace('_', ''), 'GD', dataset, epochs_gd, epochs_gd, label, values['AP'], values['total_positives'], values['TP'], values['FP']]]
 
-    for label, values in sorted(complete_metrics.items(), key=lambda v: v[0]):
-        results_complete += [[house.replace('_', ''), 'GD', dataset, epochs_gd, epochs_gd, label, values['total_positives'], values['TP'], values['FP'], values['TPm'], values['FPm'], values['FPiou']]]
+    for (iou_threshold, confidence_threshold), complete_metric in complete_metrics.items():
+        for label, values in sorted(complete_metric.items(), key=lambda v: v[0]):
+            results_complete += [[iou_threshold, confidence_threshold, house.replace('_', ''), 'GD', dataset, epochs_gd, epochs_gd, label, values['total_positives'], values['TP'], values['FP'], values['TPm'], values['FPm'], values['FPiou']]]
 
 
 for house, dataset, epochs_gd, epochs_qd, fine_tune in [(h, d, e, eq, ft) for h in houses for d in datasets for e in [60] for eq in epochs_qualified_detector for ft in  fine_tune_quantity]:
@@ -106,10 +108,12 @@ for house, dataset, epochs_gd, epochs_qd, fine_tune in [(h, d, e, eq, ft) for h 
 
     metrics, complete_metrics = compute_results(model_name, data_loader_test, COLORS)
 
-    for label, values in sorted(metrics['per_bbox'].items(), key=lambda v: v[0]):
-        results += [[house.replace('_', ''), 'QD_' + str(fine_tune), dataset, epochs_gd, epochs_qd, label, values['AP'], values['total_positives'], values['TP'], values['FP']]]
+    for (iou_threshold, confidence_threshold), metric in metrics.items():
+        for label, values in sorted(metric['per_bbox'].items(), key=lambda v: v[0]):
+            results += [[iou_threshold, confidence_threshold, house.replace('_', ''), 'QD_' + str(fine_tune), dataset, epochs_gd, epochs_qd, label, values['AP'], values['total_positives'], values['TP'], values['FP']]]
 
-    for label, values in sorted(complete_metrics.items(), key=lambda v: v[0]):
-        results_complete += [[house.replace('_', ''), 'QD_' + str(fine_tune), dataset, epochs_gd, epochs_qd, label, values['total_positives'], values['TP'], values['FP'], values['TPm'], values['FPm'], values['FPiou']]]
+    for (iou_threshold, confidence_threshold), complete_metric in complete_metrics.items():
+        for label, values in sorted(complete_metric.items(), key=lambda v: v[0]):
+            results_complete += [[iou_threshold, confidence_threshold, house.replace('_', ''), 'QD_' + str(fine_tune), dataset, epochs_gd, epochs_qd, label, values['total_positives'], values['TP'], values['FP'], values['TPm'], values['FPm'], values['FPiou']]]
 
 save_file(results, results_complete, 'detr_ap_real_data.xlsx', 'detr_complete_metrics_real_data.xlsx')