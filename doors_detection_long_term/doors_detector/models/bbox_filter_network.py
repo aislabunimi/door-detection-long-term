f6119ed72f7f56dba66c81865e32c17550a9916e
antonazzi <micheleantonazzi@gmail.com>
2023-07-22 20:27:19 +0200

    added bbox filter network geometric

--- doors_detection_long_term/doors_detector/models/bbox_filter_network.py
+++ doors_detection_long_term/doors_detector/models/bbox_filter_network.py
@@ -5,76 +5,42 @@ import torchvision.models
 from torch import nn
 from torch.nn import Identity, AdaptiveAvgPool2d
 
-from doors_detection_long_term.doors_detector.dataset.torch_dataset import DATASET
+from doors_detection_long_term.doors_detector.dataset.torch_dataset import DATASET, FINAL_DOORS_DATASET
 from doors_detection_long_term.doors_detector.models.generic_model import GenericModel, DESCRIPTION
-from doors_detection_long_term.doors_detector.models.model_names import ModelName
+from doors_detection_long_term.doors_detector.models.model_names import *
 from doors_detection_long_term.scripts.doors_detector.dataset_configurator import trained_models_path
 
 TEST: DESCRIPTION = 0
-class BboxFilterNetwork(GenericModel):
-    def __init__(self, model_name: ModelName, pretrained: bool, num_bboxes: int, dataset_name: DATASET, description: DESCRIPTION):
-        super(BboxFilterNetwork, self).__init__(model_name, dataset_name, description)
-        self._num_bboxes = num_bboxes
 
-        self.backbone = torchvision.models.resnet50(pretrained=True)
-        self.backbone.layer4 = nn.Sequential(*list(self.backbone.layer4.children())[:-2])
-        self.backbone.fc = nn.Identity()
+class SharedMLP(nn.Module):
+    def __init__(self, channels, last_activation=nn.ReLU()):
+        super(SharedMLP, self).__init__()
+        layers = []
+        for in_channels, out_channels in zip(channels[:-1], channels[1:]):
+            layers += [nn.Conv1d(in_channels=in_channels, out_channels=out_channels, kernel_size=1),
+                       nn.BatchNorm1d(num_features=out_channels),
+                       nn.ReLU()]
+        layers[-1] = last_activation
+        self.shared_mlp = nn.Sequential(*layers)
 
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=1,
-                out_channels=self._num_bboxes,
-                kernel_size=(3, 5)
-            ),
-            nn.BatchNorm2d(num_features=self._num_bboxes),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(3, 5)),
-        )
+    def forward(self, input):
+        return self.shared_mlp(input)
 
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=self._num_bboxes,
-                out_channels=self._num_bboxes,
-                kernel_size=(3, 5)
-            ),
-            nn.BatchNorm2d(num_features=self._num_bboxes),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(3, 5)),
-        )
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=self._num_bboxes,
-                out_channels=self._num_bboxes,
-                kernel_size=(3, 5)
-            ),
-            nn.BatchNorm2d(num_features=self._num_bboxes),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(3, 5)),
-        )
-        self.conv4 = nn.Sequential(
-            nn.Conv1d(
-                in_channels=self._num_bboxes,
-                out_channels=self._num_bboxes,
-                kernel_size=3
-            ),
-            nn.BatchNorm1d(num_features=self._num_bboxes),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
-        )
-        self.conv5 = nn.Sequential(
-            nn.Conv1d(
-                in_channels=self._num_bboxes,
-                out_channels=self._num_bboxes,
-                kernel_size=3
-            ),
-            nn.BatchNorm1d(num_features=self._num_bboxes),
-            nn.ReLU(),
-            nn.MaxPool1d(kernel_size=2)
-        )
-        self.output_layer = nn.Sequential(
-            nn.AdaptiveAvgPool1d(1),
-            nn.Sigmoid()
-        )
+class BboxFilterNetworkGeometric(GenericModel):
+    def __init__(self, model_name: ModelName, pretrained: bool, initial_channels: int, n_labels: int, dataset_name: DATASET, description: DESCRIPTION):
+        super(BboxFilterNetworkGeometric, self).__init__(model_name, dataset_name, description)
+        self._initial_channels = initial_channels
+
+        self.shared_mlp_1 = SharedMLP(channels=[initial_channels, 16, 32, 64])
+        self.shared_mlp_2 = SharedMLP(channels=[64, 64, 128, 256])
+        self.shared_mlp_3 = SharedMLP(channels=[256, 256, 512, 1024])
+
+        self.shared_mlp_4 = SharedMLP(channels=[1024 + 256 + 64, 512, 256, 128])
+
+        self.shared_mlp_5 = SharedMLP(channels=[128, 64, 32, 16, 1], last_activation=nn.Sigmoid())
+
+        self.shared_mlp_6 = SharedMLP(channels=[128, 64, 32, 16, n_labels], last_activation=nn.Softmax(dim=1))
+
 
         if pretrained:
             if pretrained:
@@ -85,19 +51,30 @@ class BboxFilterNetwork(GenericModel):
                 path = os.path.join(trained_models_path, path)
             self.load_state_dict(torch.load(os.path.join(path, 'model.pth'), map_location=torch.device('cpu')))
 
-    def forward(self, images, boxes):
-        x = self.backbone(images)
-        x = x.repeat(1, 64)
-        x = torch.reshape(x, (*(x.size()[:-1]), 1, 64, 2048))
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = x.squeeze(2)
-        x = torch.cat((x, boxes), 2)
-        x = self.conv4(x)
-        x = self.conv5(x)
-        x = self.output_layer(x).squeeze(2)
-        return x
+    def forward(self, images, bboxes):
+        local_features_1 = self.shared_mlp_1(bboxes)
+        local_features_2 = self.shared_mlp_2(local_features_1)
+        local_features_3 = self.shared_mlp_3(local_features_2)
+
+        global_features_1 = nn.MaxPool1d(local_features_2.size(-1))(local_features_2)
+        global_features_1 = nn.Flatten(1)(global_features_1).unsqueeze(-1)
+        global_features_1 = global_features_1.repeat(1, 1, local_features_1.size(-1))
+
+        global_features_2 = nn.MaxPool1d(local_features_3.size(-1))(local_features_3)
+        global_features_2 = nn.Flatten(1)(global_features_2).unsqueeze(-1)
+        global_features_2 = global_features_2.repeat(1, 1, local_features_1.size(-1))
+
+        mixed_features = torch.cat([local_features_1, global_features_1, global_features_2], 1)
+
+        mixed_features = self.shared_mlp_4(mixed_features)
+
+        score_features = self.shared_mlp_5(mixed_features)
+        label_features = self.shared_mlp_6(mixed_features)
+
+        score_features = torch.squeeze(score_features)
+        label_features = torch.transpose(label_features, 1, 2)
+
+        return score_features, label_features
 
 class BboxFilterNetworkLoss(nn.Module):
 
@@ -126,3 +103,11 @@ class BboxFilterNetworkLoss(nn.Module):
         elif self._reduction_global == 'sum':
             return torch.sum(reduction_on_image)
 
+
+bboxes = torch.rand((2, 7, 50))
+
+model = BboxFilterNetworkGeometric(initial_channels=7, n_labels=3, model_name=BBOX_FILTER_NETWORK_GEOMETRIC, pretrained=False, dataset_name=FINAL_DOORS_DATASET, description=TEST)
+
+scores, labels = model(torch.tensor([]), bboxes)
+
+print(scores.size(), labels.size(), labels)
