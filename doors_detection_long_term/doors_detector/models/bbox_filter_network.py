4bc414cb3cb2f41b5917b2b182acec789d8bf6b2
antonazzi <micheleantonazzi@gmail.com>
2023-09-17 01:37:54 +0200

    fixed new bbox dataset

--- doors_detection_long_term/doors_detector/models/bbox_filter_network.py
+++ doors_detection_long_term/doors_detector/models/bbox_filter_network.py
@@ -174,24 +174,24 @@ class ImageGridNetwork(GenericModel):
         self._image_grid_dimensions = image_grid_dimensions
         self.fpn = ResNet50FPN(channels=fpn_channels)
 
-        self.conv_x1 = nn.Sequential(
-            nn.AdaptiveMaxPool2d(output_size=image_grid_dimensions[0]),
-            MultipleConvolutions(original_size=256, start_size=256, end_size=64)
-        )
+        #self.conv_x1 = nn.Sequential(
+         #   nn.AdaptiveMaxPool2d(output_size=image_grid_dimensions[0]),
+          #  MultipleConvolutions(original_size=256, start_size=256, end_size=64)
+        #)
         self.conv_x2 = nn.Sequential(
             nn.AdaptiveMaxPool2d(output_size=image_grid_dimensions[1]),
-            MultipleConvolutions(original_size=512, start_size=256, end_size=64),
-            nn.Upsample(size=image_grid_dimensions[0], mode='nearest')
+            MultipleConvolutions(original_size=512, start_size=512, end_size=128),
+            nn.Upsample(size=image_grid_dimensions[1], mode='nearest')
         )
         self.conv_x3 = nn.Sequential(
             nn.AdaptiveMaxPool2d(output_size=image_grid_dimensions[2]),
-            MultipleConvolutions(original_size=1024, start_size=256, end_size=64),
-            nn.Upsample(size=image_grid_dimensions[0], mode='nearest')
+            MultipleConvolutions(original_size=1024, start_size=512, end_size=64),
+            nn.Upsample(size=image_grid_dimensions[1], mode='nearest')
         )
         self.conv_x4 = nn.Sequential(
             nn.AdaptiveMaxPool2d(output_size=image_grid_dimensions[3]),
-            MultipleConvolutions(original_size=2048, start_size=256, end_size=64),
-            nn.Upsample(size=image_grid_dimensions[0], mode='nearest')
+            MultipleConvolutions(original_size=2048, start_size=512, end_size=64),
+            nn.Upsample(size=image_grid_dimensions[1], mode='nearest')
         )
 
         self.final_convolution = nn.Sequential(
@@ -239,12 +239,12 @@ class ImageGridNetwork(GenericModel):
 
     def forward(self, images):
         multi_scales_maps = self.fpn(images)
-        x1 = self.conv_x1(multi_scales_maps['x1'])
+        #x1 = self.conv_x1(multi_scales_maps['x1'])
         x2 = self.conv_x2(multi_scales_maps['x2'])
         x3 = self.conv_x3(multi_scales_maps['x3'])
         x4 = self.conv_x4(multi_scales_maps['x4'])
 
-        x = torch.cat([x1, x2, x3, x4], dim=1)
+        x = torch.cat([x2, x3, x4], dim=1)
         image_region_features = self.final_convolution(x).squeeze(1)
         image_region_features = image_region_features.transpose(1, 2)
 
@@ -263,7 +263,7 @@ class ImageGridNetworkLoss(nn.Module):
         loss_background = torch.stack(loss_background)
         loss_target = torch.stack(loss_target)
 
-        loss = torch.mean(loss_background, dim=0) + torch.mean(loss_target, dim=0)
+        loss = 0.5*torch.mean(loss_background, dim=0) + torch.mean(loss_target, dim=0)
         return loss
 """
 image = torch.rand(4, 3, 240, 320)
