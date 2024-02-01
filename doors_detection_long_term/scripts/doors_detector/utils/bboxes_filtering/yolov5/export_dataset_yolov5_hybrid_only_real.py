1ffe75db6f23a1d8bcecfbad4052e127c5a8b6b0
micheleantonazzi <m7242872428>
2024-02-01 11:26:44 +0100

    added script to export only real data

---
+++ doors_detection_long_term/scripts/doors_detector/utils/bboxes_filtering/yolov5/export_dataset_yolov5_hybrid_only_real.py
@@ -0,0 +1,45 @@
+import cv2
+import torch.optim
+from matplotlib import pyplot as plt
+from torch import optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from doors_detection_long_term.doors_detector.dataset.dataset_bboxes.DatasetCreatorBBoxes import DatasetCreatorBBoxes, \
+    ExampleType
+from doors_detection_long_term.doors_detector.dataset.torch_dataset import FINAL_DOORS_DATASET
+from doors_detection_long_term.doors_detector.evaluators.my_evaluator import MyEvaluator
+from doors_detection_long_term.doors_detector.evaluators.my_evaluators_complete_metric import MyEvaluatorCompleteMetric
+from doors_detection_long_term.doors_detector.models.model_names import YOLOv5
+from doors_detection_long_term.doors_detector.models.yolov5 import *
+from doors_detection_long_term.doors_detector.models.yolov5_repo.utils.general import non_max_suppression
+from doors_detection_long_term.doors_detector.utilities.collate_fn_functions import collate_fn_yolov5, collate_fn_bboxes
+from doors_detection_long_term.scripts.doors_detector.dataset_configurator import *
+
+colors = {0: (0, 0, 255), 1: (0, 255, 0)}
+num_bboxes = 20
+
+for env in ['floor1', 'floor4', 'chemistry_floor0', 'house_matteo']:
+    dataset_creator_bboxes = DatasetCreatorBBoxes()
+    dataset_creator_bboxes.set_folder_name('yolov5_general_detector_hybrid_only_real' + env)
+
+    train, validation, labels, _ = get_final_doors_dataset_real_hybrid(folder_name_to_exclude=env)
+    data_loader_train = DataLoader(train, batch_size=1, collate_fn=collate_fn_yolov5, shuffle=False, num_workers=4)
+
+    data_loader_test = DataLoader(validation, batch_size=1, collate_fn=collate_fn_yolov5, shuffle=False, num_workers=4)
+    model = YOLOv5Model(model_name=YOLOv5, n_labels=len(labels.keys()), pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=EXP_GENERAL_DETECTOR_GIBSON_DEEP_DOORS_2_60_EPOCHS)
+    model.to('cuda')
+    model.eval()
+    with torch.no_grad():
+        for images, targets, converted_boxes in tqdm(data_loader_train, total=len(data_loader_train)):
+            images = images.to('cuda')
+            preds, train_out = model.model(images)
+            dataset_creator_bboxes.add_yolo_bboxes(images, targets, preds, ExampleType.TRAINING)
+
+        for images, targets, converted_boxes in tqdm(data_loader_test, total=len(data_loader_test)):
+            images = images.to('cuda')
+            preds, train_out = model.model(images)
+
+            dataset_creator_bboxes.add_yolo_bboxes(images, targets, preds, ExampleType.TEST)
+
+        dataset_creator_bboxes.export_dataset()
