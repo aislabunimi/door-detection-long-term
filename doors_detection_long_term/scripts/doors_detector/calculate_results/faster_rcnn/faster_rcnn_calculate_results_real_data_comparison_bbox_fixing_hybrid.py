2a6ce48281e3501772410b1cc5b9020b15c7ebd7
antonazzi <micheleantonazzi@gmail.com>
2024-02-06 15:04:20 +0100

    added scripts

---
+++ doors_detection_long_term/scripts/doors_detector/calculate_results/faster_rcnn/faster_rcnn_calculate_results_real_data_comparison_bbox_fixing_hybrid.py
@@ -0,0 +1,112 @@
+import numpy as np
+import pandas as pd
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from doors_detection_long_term.doors_detector.evaluators.my_evaluators_complete_metric import MyEvaluatorCompleteMetric
+from doors_detection_long_term.doors_detector.models.faster_rcnn import *
+from doors_detection_long_term.doors_detector.dataset.torch_dataset import FINAL_DOORS_DATASET
+from doors_detection_long_term.doors_detector.evaluators.my_evaluator import MyEvaluator
+from doors_detection_long_term.doors_detector.models.model_names import FASTER_RCNN
+from doors_detection_long_term.doors_detector.utilities.collate_fn_functions import collate_fn_faster_rcnn, seed_everything
+from doors_detection_long_term.scripts.doors_detector.dataset_configurator import *
+
+houses = ['floor1',]
+datasets = ['gibson_deep_doors_2']
+epochs_general_detector = [60]
+epochs_qualified_detector = [20, 40]
+fine_tune_quantity = [15, 25, 50, 75]
+device = 'cuda'
+
+seed_everything(seed=0)
+
+def compute_results(model_name, data_loader_test, description):
+    model = FasterRCNN(model_name=FASTER_RCNN, n_labels=3, pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=model_name, box_score_thresh=0.0, box_nms_thresh=1.0, box_detections_per_img=300)
+    model.eval()
+    model.to(device)
+
+    evaluator = MyEvaluator()
+    evaluator_complete_metric = MyEvaluatorCompleteMetric()
+
+
+    with torch.no_grad():
+        for images, targets, converted_boxes in tqdm(data_loader_test, total=len(data_loader_test), desc=description):
+            images = images.to(device)
+            preds = model.model(images)
+            preds = [apply_nms(pred, iou_thresh=0.5, confidence_threshold=0.01) for pred in preds]
+            for pred in preds:
+                pred['labels'] = pred['labels'] - 1
+                n=50
+                """scores, indices = torch.sort(pred['scores'], descending=True)
+                pred['scores'] = scores[:n]
+                pred['labels'] = pred['labels'][indices][:n]
+                pred['boxes'] = pred['boxes'][indices][:n]
+                """
+            evaluator.add_predictions_faster_rcnn(targets=targets, predictions=preds, img_size=images.size()[2:][::-1])
+            evaluator_complete_metric.add_predictions_faster_rcnn(targets=targets, predictions=preds, img_size=images.size()[2:][::-1])
+
+    complete_metrics = {}
+    metrics = {}
+    iou_threshold = 0.5
+    confidence_threshold = 0.750
+
+    iou_threshold = round(iou_threshold, 2)
+    confidence_threshold = round(confidence_threshold, 2)
+    metrics[(iou_threshold, confidence_threshold)] = evaluator.get_metrics(iou_threshold=iou_threshold, confidence_threshold=confidence_threshold, door_no_door_task=False, plot_curves=False)
+    complete_metrics[(iou_threshold, confidence_threshold)] = evaluator_complete_metric.get_metrics(iou_threshold=iou_threshold, confidence_threshold=confidence_threshold, door_no_door_task=False, plot_curves=False)
+
+    return metrics, complete_metrics
+
+
+def save_file(results, complete_results, file_name_1, file_name_2):
+
+    results = np.array(results).T
+    columns = ['iou_threshold', 'confidence_threshold', 'house', 'detector',  'label',  'AP', 'total_positives', 'TP', 'FP']
+    d = {}
+    for i, column in enumerate(columns):
+        d[column] = results[i]
+
+    dataframe = pd.DataFrame(d)
+
+    with pd.ExcelWriter('./../../../results/' + file_name_1) as writer:
+        if not dataframe.index.name:
+            dataframe.index.name = 'Index'
+        dataframe.to_excel(writer, sheet_name='s')
+
+    complete_results = np.array(complete_results).T
+    columns = ['iou_threshold', 'confidence_threshold', 'house', 'detector', 'label',  'total_positives', 'TP', 'FP', 'TPm', 'FPm', 'FPiou']
+    d = {}
+    for i, column in enumerate(columns):
+        d[column] = complete_results[i]
+
+    dataframe = pd.DataFrame(d)
+
+    with pd.ExcelWriter('./../../../results/' + file_name_2) as writer:
+        if not dataframe.index.name:
+            dataframe.index.name = 'Index'
+        dataframe.to_excel(writer, sheet_name='s')
+
+#model_names_qualified_detectors = [(globals()[f'EXP_2_{house}_{dataset}_EPOCHS_GD_{epochs_general}_EPOCHS_QD_{epochs_qualified}_FINE_TUNE_{quantity}'.upper()], house, dataset, quantity, epochs_general, epochs_qualified) for house in houses for dataset in datasets for quantity in fine_tune_quantity for epochs_general in epochs_general_detector for epochs_qualified in epochs_qualified_detector]
+model_names_qualified_detectors =[]
+results = []
+results_complete = []
+
+# General detectors
+
+
+for house in houses:
+    _, test, _, _ = get_final_doors_dataset_real_data(folder_name=house, train_size=0.25)
+    data_loader_test = DataLoader(test, batch_size=1, collate_fn=collate_fn_faster_rcnn, drop_last=False, num_workers=4)
+    model_name = globals()[f'EXP_GENERAL_DETECTOR_HYBRID_{house}_60_EPOCHS'.upper()]
+    metrics, complete_metrics = compute_results(model_name, data_loader_test, f'Test on {house}')
+
+    for (iou_threshold, confidence_threshold), metric in metrics.items():
+        for label, values in sorted(metric['per_bbox'].items(), key=lambda v: v[0]):
+            results += [[iou_threshold, confidence_threshold, house, 'GD', label, values['AP'], values['total_positives'], values['TP'], values['FP']]]
+
+    for (iou_threshold, confidence_threshold), complete_metric in complete_metrics.items():
+        for label, values in sorted(complete_metric.items(), key=lambda v: v[0]):
+            results_complete += [[iou_threshold, confidence_threshold, house.replace('_', ''), 'GD', label, values['total_positives'], values['TP'], values['FP'], values['TPm'], values['FPm'], values['FPiou']]]
+
+
+save_file(results, results_complete, 'faster_rcnn_ap_real_data_comparison_hybrid.xlsx', 'faster_rcnn_complete_metric_real_data_comparison_hynrid.xlsx')
