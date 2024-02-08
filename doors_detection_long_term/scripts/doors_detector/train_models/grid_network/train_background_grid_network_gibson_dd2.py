ecf7c603ec72860604513774ae62534d4b0a8759
antonazzi <micheleantonazzi@gmail.com>
2024-02-08 09:47:07 +0100

    added results

--- doors_detection_long_term/scripts/doors_detector/train_models/grid_network/train_background_grid_network_gibson_dd2.py
+++ doors_detection_long_term/scripts/doors_detector/train_models/grid_network/train_background_grid_network_gibson_dd2.py
@@ -72,8 +72,8 @@ train_accuracy = {0: [], 1: []}
 test_accuracy = {0: [], 1: []}
 real_world_accuracy = {h: {0: [], 1: []} for h in houses}
 
-for epoch in range(20):
-    scheduler.step()
+for epoch in range(60):
+    #scheduler.step()
     bbox_model.train()
     criterion.train()
     optimizer.zero_grad()
