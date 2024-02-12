3aa1487298f57c65805f99373835fe0d7dbf6f59
antonazzi <micheleantonazzi@gmail.com>
2024-02-12 11:39:16 +0100

    fixed export scripts

--- doors_detection_long_term/scripts/doors_detector/utils/bboxes_filtering/faster_rcnn/export_dataset_real_world_trained_with_dd2.py
+++ doors_detection_long_term/scripts/doors_detector/utils/bboxes_filtering/faster_rcnn/export_dataset_real_world_trained_with_dd2.py
@@ -22,37 +22,38 @@ num_bboxes = 20
 houses = ['floor1', 'floor4', 'chemistry_floor0', 'house_matteo']
 
 for house in houses:
+    for quantity in [0.15, 0.25, 0.50, 0.75]:
 
-    dataset_creator_bboxes = DatasetCreatorBBoxes()
-    dataset_creator_bboxes.set_folder_name('faster_rcnn_general_detector_gibson_dd2_' + house)
+        dataset_creator_bboxes = DatasetCreatorBBoxes()
+        dataset_creator_bboxes.set_folder_name(f'faster_rcnn_general_detector_gibson_dd2_{house}_{quantity}')
 
-    train, test, l, _ = get_final_doors_dataset_real_data(folder_name=house, train_size=0.75, transform_train=False)
-    labels = l
-    data_loader_train = DataLoader(train, batch_size=1, collate_fn=collate_fn_faster_rcnn, drop_last=False, num_workers=4)
-    data_loader_test = DataLoader(test, batch_size=1, collate_fn=collate_fn_faster_rcnn, drop_last=False, num_workers=4)
+        train, test, l, _ = get_final_doors_dataset_real_data(folder_name=house, train_size=quantity, transform_train=False)
+        labels = l
+        data_loader_train = DataLoader(train, batch_size=1, collate_fn=collate_fn_faster_rcnn, drop_last=False, num_workers=4)
+        data_loader_test = DataLoader(test, batch_size=1, collate_fn=collate_fn_faster_rcnn, drop_last=False, num_workers=4)
 
-    model = FasterRCNN(model_name=FASTER_RCNN, n_labels=3, pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=EXP_GENERAL_DETECTOR_GIBSON_DEEP_DOORS_2_60_EPOCHS,
-                       box_score_thresh=0.0, box_nms_thresh=1.0, box_detections_per_img=300)
-    model.to('cuda')
-    model.model.eval()
-    ap_metric_classic = {0: 0, 1: 0}
-    complete_metric_classic = {'TP': 0, 'FP': 0, 'BFD': 0}
-    with torch.no_grad():
-        for images, targets, converted_boxes in tqdm(data_loader_train, total=len(data_loader_train)):
-            images = images.to('cuda')
-            preds = model.model(images)
-            for pred in preds:
-                pred['labels'] = pred['labels']-1
+        model = FasterRCNN(model_name=FASTER_RCNN, n_labels=3, pretrained=True, dataset_name=FINAL_DOORS_DATASET, description=EXP_GENERAL_DETECTOR_GIBSON_DEEP_DOORS_2_60_EPOCHS,
+                           box_score_thresh=0.0, box_nms_thresh=1.0, box_detections_per_img=300)
+        model.to('cuda')
+        model.model.eval()
+        ap_metric_classic = {0: 0, 1: 0}
+        complete_metric_classic = {'TP': 0, 'FP': 0, 'BFD': 0}
+        with torch.no_grad():
+            for images, targets, converted_boxes in tqdm(data_loader_train, total=len(data_loader_train)):
+                images = images.to('cuda')
+                preds = model.model(images)
+                for pred in preds:
+                    pred['labels'] = pred['labels']-1
 
-            dataset_creator_bboxes.add_faster_rcnn_bboxes(images, targets, preds, ExampleType.TRAINING)
+                dataset_creator_bboxes.add_faster_rcnn_bboxes(images, targets, preds, ExampleType.TRAINING)
 
-        #c = int((3/5) * len(data_loader_test))
-        for i, (images, targets, converted_boxes) in tqdm(enumerate(data_loader_test), total=len(data_loader_test)):
-            images = images.to('cuda')
-            preds = model.model(images)
-            for pred in preds:
-                pred['labels'] = pred['labels']-1
+            #c = int((3/5) * len(data_loader_test))
+            for i, (images, targets, converted_boxes) in tqdm(enumerate(data_loader_test), total=len(data_loader_test)):
+                images = images.to('cuda')
+                preds = model.model(images)
+                for pred in preds:
+                    pred['labels'] = pred['labels']-1
 
-            dataset_creator_bboxes.add_faster_rcnn_bboxes(images, targets, preds, ExampleType.TEST)
+                dataset_creator_bboxes.add_faster_rcnn_bboxes(images, targets, preds, ExampleType.TEST)
 
-    dataset_creator_bboxes.export_dataset()
+        dataset_creator_bboxes.export_dataset()
