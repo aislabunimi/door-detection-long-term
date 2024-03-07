e7ac27d81a0e5db8c63f80432b69127a833f5212
antonazzi <micheleantonazzi@gmail.com>
2024-03-07 14:58:20 +0100

    added script to export real dataset faster rcnn

---
+++ doors_detection_long_term/scripts/doors_detector/utils/usefull_script/filternet/latex_table_different_quantities_ablation.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+boxes = 100
+quantity = 0.75
+houses = ['floor1', 'floor4', 'chemistry_floor0', 'house_matteo']
+metric_ap = pd.read_csv('../../../../results/filternet_results_ap_ablation.csv')
+metric_complete = pd.read_csv('../../../../results/filternet_results_complete_ablation.csv')
+
+metric_ap = metric_ap[(metric_ap['boxes'] == boxes) & (metric_ap['quantity'] == quantity)]
+metric_complete = metric_complete[(metric_complete['boxes'] == boxes)& (metric_complete['quantity'] == quantity)]
+
+metric_ap = metric_ap.groupby(['model', 'house', 'quantity', 'boxes', 'iou_threshold_matching',
+                                           'confidence_threshold_tasknet', 'iou_threshold_tasknet',
+                                           'confidence_threshold_filternet', 'iou_threshold_filternet',
+                                            'relabeling', 'rescoring', 'suppression'], as_index=False).sum()
+
+
+metric_complete = metric_complete.groupby(['model', 'house', 'quantity', 'boxes', 'iou_threshold_matching',
+                          'confidence_threshold_tasknet', 'iou_threshold_tasknet',
+                          'confidence_threshold_filternet', 'iou_threshold_filternet',
+                                           'relabeling', 'rescoring', 'suppression'], as_index=False).sum()
+
+metric_complete['TP_p'] = metric_complete['TP'] / metric_complete['total_positives']
+metric_complete['FP_p'] = metric_complete['FP'] / metric_complete['total_positives']
+metric_complete['FPiou_p'] = metric_complete['FPiou'] / metric_complete['total_positives']
+
+table = ''
+for i, (sup, res, rel) in enumerate([(r1, r2, s) for r1 in range(2) for r2 in range(2) for s in range(2)]):
+    if len([e for e in (rel, res, sup) if e == 1]) == 2:
+        continue
+    model = 'filternet'
+    table += f" \\{'Xmark' if rel == 0 else 'Vmark'} & \\{'Xmark' if res == 0 else 'Vmark'} & \\{'Xmark' if sup == 0 else 'Vmark'} "
+    for e in houses:
+        table += (f'&{int(round(metric_ap.loc[(metric_ap["house"] == e) & (metric_ap["model"] == model) & (metric_ap["relabeling"] == rel)& (metric_ap["rescoring"] == res)& (metric_ap["suppression"] == sup), "AP"].tolist()[0]/2*100, 0))} &'
+                  f'{int(round(metric_complete.loc[(metric_complete["house"] == e) & (metric_complete["model"] == model) & (metric_complete["relabeling"] == rel)& (metric_complete["rescoring"] == res)& (metric_complete["suppression"] == sup), "TP_p"].tolist()[0]*100, 0))}\\% & '
+                  f'{int(round(metric_complete.loc[(metric_complete["house"] == e) & (metric_complete["model"] == model) & (metric_complete["relabeling"] == rel)& (metric_complete["rescoring"] == res)& (metric_complete["suppression"] == sup), "FP_p"].tolist()[0]*100, 0))}\\%  & '
+                  f'{int(round(metric_complete.loc[(metric_complete["house"] == e) & (metric_complete["model"] == model) & (metric_complete["relabeling"] == rel)& (metric_complete["rescoring"] == res)& (metric_complete["suppression"] == sup), "FPiou_p"].tolist()[0]*100, 0))}\\% ')
+    table += '\\\\\n'
+print(table)
