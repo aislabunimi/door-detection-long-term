6b92953d23be45370ced70c0cca36409cf9078a6
Matteo Princisgh <mauro.tellaroli@studenti.unimi.it>
2024-08-22 11:51:48 +0200

    added results

--- doors_detection_long_term/scripts/doors_detector/calculate_results/detr/detr_calculate_results_real_data_day_night.py
+++ doors_detection_long_term/scripts/doors_detector/calculate_results/detr/detr_calculate_results_real_data_day_night.py
@@ -42,8 +42,8 @@ def compute_results(model_name, data_loader_test):
         for confidence_threshold in np.arange(0.5, 0.96, 0.05):
             iou_threshold = round(iou_threshold, 2)
             confidence_threshold = round(confidence_threshold, 2)
-            metrics[(iou_threshold, confidence_threshold)] = evaluator.get_metrics(iou_threshold=iou_threshold, confidence_threshold=confidence_threshold, door_no_door_task=False, plot_curves=False, colors=COLORS)
-            complete_metrics[(iou_threshold, confidence_threshold)] = evaluator_complete_metric.get_metrics(iou_threshold=iou_threshold, confidence_threshold=confidence_threshold, door_no_door_task=False, plot_curves=False, colors=COLORS)
+            metrics[(iou_threshold, confidence_threshold)] = evaluator.get_metrics(iou_threshold=iou_threshold, confidence_threshold=confidence_threshold, door_no_door_task=False, plot_curves=False, colors=[])
+            complete_metrics[(iou_threshold, confidence_threshold)] = evaluator_complete_metric.get_metrics(iou_threshold=iou_threshold, confidence_threshold=confidence_threshold, door_no_door_task=False, plot_curves=False, colors=[])
 
 
     return metrics, complete_metrics
@@ -106,7 +106,7 @@ for house, dataset, epochs_gd in [(h, d, e) for h in ['floor4_run2'] for d in da
             results_complete += [[iou_threshold, confidence_threshold, house.replace('_', ''), 'GD', dataset, epochs_gd, epochs_gd, label, values['total_positives'], values['TP'], values['FP'], values['TPm'], values['FPm'], values['FPiou']]]
 
 
-for house, dataset, epochs_gd, epochs_qd, fine_tune in [(h, d, e, eq, ft) for h in houses for d in datasets for e in [60] for eq in epochs_qualified_detector for ft in  fine_tune_quantity]:
+for house, dataset, epochs_gd, epochs_qd, fine_tune in [(h, d, e, eq, ft) for h in ['floor4_run2'] for d in datasets for e in [60] for eq in epochs_qualified_detector for ft in  fine_tune_quantity]:
     train_envs = list(houses)
     train_envs.remove(house)
     train, validation, test, labels, _ = get_final_doors_dataset_real_data_multiple_houses_different_test(
